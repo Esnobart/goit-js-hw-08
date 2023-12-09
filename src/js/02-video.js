819ae52086b5cbf736282cc5da1599020d390d8f
Esnobart <offrichterrr@gmail.com>
2023-12-09 20:16:00 +0000

    update js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,28 +1,15 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+const storageKey = 'videoplayer-current-time';
 const iframe = document.getElementById('vimeo-player');
 const player = new Player(iframe);
 
-const STORAGE_KEY = 'videoplayer-current-time';
-
-const saveTimeToLocalStorage = () => {
-  player.getCurrentTime().then((time) => {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(time));
-  });
-};
-
+const saveTimeToLocalStorage = () => player.getCurrentTime().then((time) => localStorage.setItem(storageKey, JSON.stringify(time)));
 const restoreTimeFromLocalStorage = () => {
-  const savedTime = localStorage.getItem(STORAGE_KEY);
-  if (savedTime) {
-    const parsedTime = JSON.parse(savedTime);
-    player.setCurrentTime(parsedTime);
-  }
+  const savedTime = localStorage.getItem(storageKey);
+  savedTime && player.setCurrentTime(JSON.parse(savedTime));
 };
 
 player.on('timeupdate', throttle(saveTimeToLocalStorage, 1000));
-
-player.ready().then(() => {
-  restoreTimeFromLocalStorage();
-});
-
+player.ready().then(restoreTimeFromLocalStorage);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+const storageKey = 'feedback-form-state';
+
+const saveStateToLocalStorage = () => {
+  const formData = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
+  localStorage.setItem(storageKey, JSON.stringify(formData));
+};
+
+const fillFormFields = () => {
+  const savedData = localStorage.getItem(storageKey);
+  if (savedData) {
+    const parsedData = JSON.parse(savedData);
+    form.elements.email.value = parsedData.email;
+    form.elements.message.value = parsedData.message;
+  }
+};
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  console.log('Submitted:', {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  });
+  localStorage.removeItem(storageKey);
+  form.reset();
+};
+
+form.addEventListener('input', throttle(saveStateToLocalStorage, 500));
+form.addEventListener('submit', handleSubmit);
+
+document.addEventListener('DOMContentLoaded', fillFormFields);
 
