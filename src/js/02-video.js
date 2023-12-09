865915a6243e3207770998be6741ca8497ff990a
Esnobart <offrichterrr@gmail.com>
2023-12-09 20:36:18 +0000

    update js02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,7 +5,8 @@ const storageKey = 'videoplayer-current-time';
 const iframe = document.getElementById('vimeo-player');
 const player = new Player(iframe);
 
-const saveTimeToLocalStorage = () => player.getCurrentTime().then((time) => localStorage.setItem(storageKey, JSON.stringify(time)));
+const saveTimeToLocalStorage = () => player.getCurrentTime()
+    .then((time) => localStorage.setItem(storageKey, JSON.stringify(time)));
 const restoreTimeFromLocalStorage = () => {
   const savedTime = localStorage.getItem(storageKey);
   savedTime && player.setCurrentTime(JSON.parse(savedTime));
