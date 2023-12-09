f5cb364c6bf35e7eacc0e40d38708f3792bc0c77
Esnobart <offrichterrr@gmail.com>
2023-12-09 19:57:58 +0000

    update js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryDiv = document.querySelector('.gallery');
+galleryDiv.insertAdjacentHTML('beforeend', galleryItem(galleryItems));
+
+function galleryItem(arr) {
+  return arr
+    .map(({ preview, original, description }) =>
+      `<li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+          </a>
+      </li>`
+    )
+    .join('');
+}
+
+const gallery = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, });
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const saveTimeToLocalStorage = () => {
+  player.getCurrentTime().then((time) => {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(time));
+  });
+};
+
+const restoreTimeFromLocalStorage = () => {
+  const savedTime = localStorage.getItem(STORAGE_KEY);
+  if (savedTime) {
+    const parsedTime = JSON.parse(savedTime);
+    player.setCurrentTime(parsedTime);
+  }
+};
+
+player.on('timeupdate', throttle(saveTimeToLocalStorage, 1000));
+
+player.ready().then(() => {
+  restoreTimeFromLocalStorage();
+});
 
